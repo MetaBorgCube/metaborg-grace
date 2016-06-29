81ff9db6e7cff6874ea507a24871f564adb3a30e
Michiel Haisma <michielhaisma@hotmail.com>
2016-06-29 12:56:43 +0200

    fix removed base (expression, declaration etc.) and remove print before/after desugaring

--- grace/src/main/strategies/grace/strategies/DesugarTransform.java
+++ grace/src/main/strategies/grace/strategies/DesugarTransform.java
@@ -7,10 +7,6 @@ public final class DesugarTransform implements ITermTransformer {
 
     @Override
     public IStrategoTerm transform(IStrategoTerm term) {
-    	System.out.println("Invoking desugar-complete strategy on term: " + term);
-    	IStrategoTerm desugaredTerm = grace.trans.desugar_complete_0_0.instance.invoke(grace.trans.Main.init(), term);
-//    	IStrategoTerm desugaredTerm = grace.trans.desugar_test_0_0.instance.invoke(grace.trans.Main.init(), term);
-    	System.out.println("Resulted term: " + desugaredTerm);
-        return desugaredTerm; 
+    	return grace.trans.desugar_complete_0_0.instance.invoke(grace.trans.Main.init(), term); 
     }
 }
--- grace/test/program/scratchpad.grace
+++ grace/test/program/scratchpad.grace
@@ -1,11 +1,17 @@
 class super {
-  method a { base_print(5); };
+  method a { base_print(4); };
 };
 
-method a { base_print(4); };
+method out { base_print(5); };
+
+method b { out; };
 
 object {
-//  inherit super;
-//  method a { base_print(6); };
+  inherit super;
+  method c { base_print(6); };
+  method d(a, b) { base_print(a); base_print(b); };
   a;
+  b;
+  c;
+  d(7,8);
 };
