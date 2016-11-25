0ab9f05d1eefee5b22071b90c8c4c8aaa1547e39
Michiel Haisma <michielhaisma@hotmail.com>
2016-11-25 16:26:30 +0100

    add universal way to get file extensions

---
+++ grace/src/main/strategies/grace/strategies/FileExtensions.java
@@ -0,0 +1,8 @@
+package grace.strategies;
+
+public class FileExtensions {    
+    public static String[] getFileExtensions() {
+    	String[] extensions = {"grace", "grc"};
+    	return extensions;
+    }
+}
