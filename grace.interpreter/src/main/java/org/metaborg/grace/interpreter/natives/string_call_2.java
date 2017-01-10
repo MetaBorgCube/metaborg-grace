b99d94c7064dcbd09cbaa8d17bced6126f501107
Michiel Haisma <michielhaisma@hotmail.com>
2017-01-10 17:31:22 +0100

    improve bool and string natives

--- grace.interpreter/src/main/java/org/metaborg/grace/interpreter/natives/string_call_2.java
+++ grace.interpreter/src/main/java/org/metaborg/grace/interpreter/natives/string_call_2.java
@@ -1,21 +1,22 @@
 package org.metaborg.grace.interpreter.natives;
 
 import org.metaborg.grace.interpreter.generated.TypesGen;
-import org.metaborg.grace.interpreter.generated.terms.BoolV_1_Term;
 import org.metaborg.grace.interpreter.generated.terms.IVTerm;
+import org.metaborg.grace.interpreter.generated.terms.NumV_1_Term;
+import org.metaborg.grace.interpreter.generated.terms.StringV_1_Term;
 import org.metaborg.meta.lang.dynsem.interpreter.nodes.building.TermBuild;
 
 import com.oracle.truffle.api.frame.VirtualFrame;
 import com.oracle.truffle.api.source.SourceSection;
 
-public class bool_call_2 extends TermBuild {
+public class string_call_2 extends TermBuild {
 
 	@Child
 	protected TermBuild leftNode;
 	@Child
 	protected TermBuild opNode;
-
-	public bool_call_2(SourceSection source, TermBuild l, TermBuild op) {
+	
+	public string_call_2(SourceSection source, TermBuild l, TermBuild op) {
 		super(source);
 		this.leftNode = l;
 		this.opNode = op;
@@ -23,24 +24,22 @@ public class bool_call_2 extends TermBuild {
 
 	@Override
 	public IVTerm executeGeneric(VirtualFrame frame) {
-		final BoolV_1_Term left = TypesGen.asBoolV_1_Term(leftNode.executeGeneric(frame));
+		final StringV_1_Term left = TypesGen.asStringV_1_Term(leftNode.executeGeneric(frame));
 		final String op = TypesGen.asString(opNode.executeGeneric(frame));
 		switch (op) {
-		case "not":
-			return doNot(left);
-		case "prefix!":
-			return doNot(left);
+		case "size":
+			return doSize(left);
 		default:
-			throw new IllegalArgumentException("operator: '" + op + "' not recognised as operator on bool.");
+			throw new IllegalArgumentException("operator: '" + op + "' not recognised as operator on string.");
 		}
 	}
 
-	private IVTerm doNot(final BoolV_1_Term left) {
-		return new BoolV_1_Term(!left.get_1());
+	private IVTerm doSize(StringV_1_Term left) {
+		return new NumV_1_Term(left.get_1().length());
 	}
-
+	
 	public static TermBuild create(SourceSection source, TermBuild left, TermBuild op) {
-		return new bool_call_2(source, left, op);
+		return new string_call_2(source, left, op);
 	}
 
 }
