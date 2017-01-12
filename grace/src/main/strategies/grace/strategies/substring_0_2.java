61d84d5d82f6fd3d1d7bbf8d676bc396c808373f
Michiel Haisma <michielhaisma@hotmail.com>
2017-01-12 18:10:04 +0100

    interpolate string in parser and desugarer (wrong order atm)

---
+++ grace/src/main/strategies/grace/strategies/substring_0_2.java
@@ -0,0 +1,32 @@
+package grace.strategies;
+
+import org.spoofax.interpreter.core.Tools;
+import org.spoofax.interpreter.terms.IStrategoTerm;
+import org.spoofax.interpreter.terms.ITermFactory;
+import org.strategoxt.lang.Context;
+import org.strategoxt.lang.Strategy;
+
+public class substring_0_2 extends Strategy {
+    public static substring_0_2 instance = new substring_0_2();
+
+    @Override public IStrategoTerm invoke(Context context, 
+    		IStrategoTerm current, IStrategoTerm begin, IStrategoTerm end) {
+    	System.out.println("substring being called");
+        final String str = Tools.asJavaString(current);
+        final int begini = Tools.asJavaInt(begin);
+        final int endi = Tools.asJavaInt(end);
+        
+        final ITermFactory factory = context.getFactory();
+        String res;
+        try {
+        	res = str.substring(begini, endi);
+        } catch (IndexOutOfBoundsException e) {
+        	return null;
+        }
+//        IStrategoTerm after = factory.makeString("1: " + begin.getTermType() + " 2: " + end.getTermType() + " str: " + str);
+        IStrategoTerm after = factory.makeString(res);
+        after = factory.annotateTerm(after, current.getAnnotations());
+//        after = factory.copyAttachments(current, after);
+        return after;
+    }
+}
