d474656576a9293805c1c44a714cf5f5b4021f6a
Vlad Vergu <v.vergu@gmail.com>
2017-02-08 18:03:13 +0200

    Modify Java call to desugaring to use an origin tracking term factory

--- grace/src/main/strategies/grace/strategies/DesugarTransform.java
+++ grace/src/main/strategies/grace/strategies/DesugarTransform.java
@@ -2,11 +2,15 @@ package grace.strategies;
 
 import org.metaborg.meta.lang.dynsem.interpreter.terms.ITermTransformer;
 import org.spoofax.interpreter.terms.IStrategoTerm;
-    
+import org.spoofax.jsglr.client.imploder.ImploderOriginTermFactory;
+import org.spoofax.terms.TermFactory;
+import org.strategoxt.lang.Context;
+
 public final class DesugarTransform implements ITermTransformer {
 
-    @Override
-    public IStrategoTerm transform(IStrategoTerm term) {
-    	return grace.trans.desugar_complete_0_0.instance.invoke(grace.trans.Main.init(), term); 
-    }
+	@Override
+	public IStrategoTerm transform(IStrategoTerm term) {
+		return grace.trans.desugar_complete_0_0.instance
+				.invoke(grace.trans.Main.init(new Context(new ImploderOriginTermFactory(new TermFactory()))), term);
+	}
 }
